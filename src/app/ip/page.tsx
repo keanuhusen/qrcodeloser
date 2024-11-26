6aa0a6aed1fe9370dfede02e0202ceaddb021a6a
hc-keanu <keanu.husen@highlandscollege.com>
2024-11-26 14:32:20 -0800

    ip testing

--- src/app/ip/page.tsx
+++ src/app/ip/page.tsx
@@ -3,6 +3,9 @@ import { headers } from 'next/headers';
 export default async function Page() {
   const headersList = await headers();
   const ip = headersList.get('x-forwarded-for') || headersList.get('x-real-ip') || headersList.get('remote-address') || 'Unknown';
+  const ipForwarded = headersList.get('x-forwarded-for') || 'Unknown';
+  const ipReal = headersList.get('x-real-ip') || 'Unknown';
+  const ipRemote = headersList.get('remote-address') || 'Unknown';
   const res = await fetch(`http://ip-api.com/json/${ip}`);
   const ipInfo = await res.json();
   const region = ipInfo?.regionName ? ipInfo.regionName : 'Unknown';
@@ -12,6 +15,12 @@ export default async function Page() {
     <div className="block">
       <main className="py-8 px-4">
         IP address: {ip}
+        <br />
+        IP forwarded: {ipForwarded}
+        <br />
+        IP real: {ipReal}
+        <br />
+        IP remote: {ipRemote}
         <br />
         regin: {region}
         <br />
